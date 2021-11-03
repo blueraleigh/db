4740a9a0ccec56856203e9a03638f3c5def63ce6
blueraleigh <mcgrundler@gmail.com>
2021-11-03 09:36:21 -0400

    change delimiter sequence in db.subst

--- R/db-subst.R
+++ R/db-subst.R
@@ -4,27 +4,27 @@
 #' @param txt The character string for substitution.
 #' @param env The environment in which to perform the substitution.
 #' @details The input \code{text} is searched for text enclosed
-#' between two braces, which it treats as an expression and evaluates
-#' in the calling frame, substituting any returned value in place of
-#' the text between braces.
+#' between <% and %>, which it treats as a sequence of expressions and evaluates
+#' in the calling frame, substituting the value of the last evaluated
+#' expression in place of the text between delimiters.
 #' @return The input character string with the values returned from
 #' any expressions between braces substituted in place.
-#' @note Currently expressions do not nest. That is, the function will stop
-#' at the first closing brace it finds and try to parse the extracted
-#' expression. So "hello, \{\{greeting\}\}!" will attempt to parse `\{greeting`,
+#' @note Currently delimiters do not nest. That is, the function will stop
+#' at the first closing delimiter sequence it finds and try to parse the extracted
+#' expression. So "hello, <%<%greeting%>%>!" will attempt to parse `<%greeting`,
 #' which will return a parse error.
 #' @examples
 #' db = db.open()
 #' greeting = "world!"
-#' db.subst(db, "hello, {greeting}!") # => hello, world!!
+#' db.subst(db, "hello, <% greeting %>!") # => hello, world!!
 #' db.subst(
 #'   db
-#'   , "hello, {greeting} what a grand {greeting}"
+#'   , "hello, <%greeting%> what a grand <%greeting%>"
 #' ) # => hello, world! what a grand world!
 #' bar = function() return ("bar")
-#' db.subst(db, "foo {bar()}") # => foo bar
-#' db.subst(db, "foo {bar}") # => foo # note a trailing space
-#' db.subst(db, "foo {paste0(deparse(bar), collapse='')}") # => foo function () return(\"bar\")
+#' db.subst(db, "foo <% bar() %>") # => foo bar
+#' db.subst(db, "foo <% bar %>") # => foo # note a trailing space
+#' db.subst(db, "foo <% paste0(deparse(bar), collapse='') %>") # => foo function () return(\"bar\")
 #' db.close(db)
 #' @export
 db.subst = function(db, text, env=parent.frame()) {
--- src/db_subst.c
+++ src/db_subst.c
@@ -4,6 +4,8 @@
 
 #include "sqlite3.h"
 
+/*
+previous version that used { and } as delimiters
 
 SEXP db_subst(SEXP db, SEXP txt, SEXP env) {
 
@@ -48,6 +50,102 @@ SEXP db_subst(SEXP db, SEXP txt, SEXP env) {
                 sqlite3_str_reset(e);
                 UNPROTECT(4);
                 break;
+            default:
+                sqlite3_str_appendchar(s, 1, c);
+                break;
+        }
+    }
+
+    unused = sqlite3_str_finish(e);
+    sqlite3_free(unused);
+    ret = sqlite3_str_finish(s);
+    if (ret)
+        result = PROTECT(mkString(ret));
+    else
+        result = PROTECT(mkString(""));
+    sqlite3_free(ret);
+    UNPROTECT(1);
+    return result;
+
+    badparse:
+        unused = sqlite3_str_finish(e);
+        ret = sqlite3_str_finish(s);
+        sqlite3_free(unused);
+        sqlite3_free(ret);
+        if (status != PARSE_OK)
+            error("could not parse template string");
+        return R_NilValue;
+}
+*/
+
+
+
+static int in_delim(int i, const char *str)
+{
+    if (str[i++] == '%' && str[i] == '>')
+        return 0;
+    return 1;
+}
+
+
+// new version that uses <% and %> as delimiters like the R package brew
+SEXP db_subst(SEXP db, SEXP txt, SEXP env) {
+
+    char c;
+    char *ret;
+    char *unused;
+    const char *cs = CHAR(STRING_ELT(txt, 0));
+    ParseStatus status;
+
+    sqlite3_str *s = sqlite3_str_new((sqlite3 *)R_ExternalPtrAddr(db));
+    sqlite3_str *e = sqlite3_str_new((sqlite3 *)R_ExternalPtrAddr(db));
+
+    SEXP p, q;
+    SEXP expr;
+    SEXP result;
+
+    int i = 0;
+    int j;
+    int n = strlen(cs);
+    int err;
+    while (i < n) {
+        c = cs[i++];
+        switch (c) {
+            case '<':
+                if (cs[i] == '%') {
+                    // discard initial delim
+                    ++i;
+                    // get value between delims
+                    while (i < n && in_delim(i, cs))
+                        sqlite3_str_appendchar(e, 1, cs[i++]);
+                    // discard final delims
+                    ++i, ++i;
+
+                    p = PROTECT(mkString(sqlite3_str_value(e)));
+                    expr = PROTECT(R_ParseVector(p, -1, &status, R_NilValue));
+                    if (status != PARSE_OK) {
+                        UNPROTECT(2);
+                        goto badparse;
+                    }
+                    for (j = 0; j < (length(expr)-1); ++j) {
+                        q = PROTECT(VECTOR_ELT(expr, j));
+                        R_tryEval(q, env, &err);
+                        if (err) {
+                            UNPROTECT(3);
+                            goto badparse;
+                        }
+                        UNPROTECT(1);
+                    }
+                    // use result of last expression
+                    q = PROTECT(VECTOR_ELT(expr, j));
+                    result = PROTECT(R_tryEval(q, env, &err));
+                    if (TYPEOF(result) == STRSXP)
+                        sqlite3_str_appendall(s, CHAR(STRING_ELT(result, 0)));
+                    sqlite3_str_reset(e);
+                    UNPROTECT(4);
+                    break;
+                }
+                // else intentional fall through
             default:
                 sqlite3_str_appendchar(s, 1, c);
                 break;
